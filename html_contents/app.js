6d075b4a1741ee4611ea63bf07ee9b4ac5dcda2f
Guillaume <guillaume.thouvenin@vates.tech>
2024-06-28 10:40:32 +0200

    add the import of wasm file

--- html_contents/app.js
+++ html_contents/app.js
@@ -34,7 +34,31 @@ function drawCircleInBox(canvasId) {
     context.stroke();
 }
 
+
+async function load_wasm() {
+  // https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/instantiate_static
+  const importObject = {
+    imports: {
+      ilog(arg) { // ilog stands for (i)mported (log)
+        console.log(arg);
+      },
+    },
+  };
+
+  const response = await fetch('add.wasm');
+  const bytes = await response.arrayBuffer();
+  const buffer = await WebAssembly.instantiate(bytes, importObject);
+  const exports = buffer.instance.exports;
+
+  console.log(exports);
+
+  const answer_to_life = exports.add(30, 12);
+  document.getElementById('answer').textContent = `The answer to life, the universe, and everything is ${answer_to_life}`;
+}
+
 // Call the function when the window loads
 window.onload = () => {
-    drawCircleInBox('myCanvas');
+    load_wasm();
+    drawCircleInBox('canvas_1');
+    drawCircleInBox('canvas_2');
 };
